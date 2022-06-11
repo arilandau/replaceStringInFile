16c000ce5201fe24f96a4a116822e2256e194f99
ARILANDAU <ariellelandau360@gmail.com>
2022-06-10 22:39:50 -0400

    add manifest and clean up

--- ReplaceInFiles/src/ReplaceInFiles/ReplaceStringInFile.java
+++ ReplaceInFiles/src/ReplaceInFiles/ReplaceStringInFile.java
@@ -10,76 +10,75 @@ import java.util.regex.Pattern;
 public class ReplaceStringInFile {
 
 	public static void main(String[] args) throws IOException {
-		
-		
-//		String delimiter = args[0];
-		
+
+		String delimiter = args[0];
 		String fileToReadPath = args[1];
 		String fileToWritePath = args[2];
-		
+
 		File fileToRead = new File(fileToReadPath);
-		Scanner scannerToRead = new Scanner(fileToRead);  
-//		Scanner scannerToRead = new Scanner(fileToRead).useDelimiter(delimiter);  
-		
+		Scanner scannerToRead = new Scanner(fileToRead);
+
 		FileWriter fileToWrite = new FileWriter(fileToWritePath);
-		                
-        loopLines(scannerToRead, fileToWrite);
-        
-        File fileToCountLines = new File(fileToReadPath);
-        Scanner scannerToCountLines = new Scanner(fileToCountLines);
-        
-        countLines(scannerToCountLines, fileToWrite);
-        
-        fileToWrite.close();
-        scannerToRead.close();
-        scannerToCountLines.close();       
+
+		Pattern pattern = Pattern.compile(delimiter);
+
+		loopLines(scannerToRead, fileToWrite, pattern);
+
+		File fileToCountLines = new File(fileToReadPath);
+		Scanner scannerToCountLines = new Scanner(fileToCountLines);
+
+		countLines(scannerToCountLines, fileToWrite);
+
+		fileToWrite.close();
+		scannerToRead.close();
+		scannerToCountLines.close();
 	}
 
-	private static void loopLines(Scanner scannerToRead, FileWriter fileToWrite) throws IOException {
-		
+	private static void loopLines(Scanner scannerToRead, FileWriter fileToWrite, Pattern pattern) throws IOException {
+
 		int countMatches = 0;
-	    boolean splitString = true;
-		Pattern pattern = Pattern.compile("NUMBER OF VISITORS: ");
-		
-		while (scannerToRead.hasNext()) {						
+		boolean splitString = true;
+
+		while (scannerToRead.hasNext()) {
+			
 			String currentSection = scannerToRead.nextLine();
 
-			if (splitString) {								
+			if (splitString) {
 				Matcher matcher = pattern.matcher(currentSection);
 				boolean matchFound = matcher.find();
-
+				
 				fileToWrite.append(currentSection);
 				System.out.println(currentSection);
-				
+
 				if (matchFound) {
 					splitString = false;
-				}			
-				
+				}
+
 			} else {
 				fileToWrite.append(currentSection);
 				fileToWrite.append("\n");
 			}
-			
-			countMatches++;		
+
+			countMatches++;
 		}
-		
-		fileToWrite.append("\n\n>>>>>>>>>>>>>>><<<<<<<<<<<<<<<"); 
-		fileToWrite.append("\n\nTotal Matches: " + String.valueOf(countMatches)); 
+
+		fileToWrite.append("\n\n>>>>>>>>>>>>>>><<<<<<<<<<<<<<<");
+		fileToWrite.append("\n\nTotal Matches: " + String.valueOf(countMatches));
 	}
 
 	private static void countLines(Scanner scannerToCountLines, FileWriter fileToWrite) throws IOException {
 
 		int countLines = 0;
-		
+
 		while (scannerToCountLines.hasNextLine()) {
-			
+
 			scannerToCountLines.nextLine();
-			
-			countLines++;	
+
+			countLines++;
 		}
-		
-		fileToWrite.append("\n\nTotal Lines: " + String.valueOf(countLines)); 		
-		fileToWrite.append("\n\n>>>>>>>>>>>>>>><<<<<<<<<<<<<<<"); 
+
+		fileToWrite.append("\n\nTotal Lines: " + String.valueOf(countLines));
+		fileToWrite.append("\n\n>>>>>>>>>>>>>>><<<<<<<<<<<<<<<");
 	}
 
 }
