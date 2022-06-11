ae06b566fea1fb808ebeb168875559ebadd3103c
ARILANDAU <ariellelandau360@gmail.com>
2022-06-11 14:02:08 -0400

    fix formatting

--- ReplaceInFiles/src/ReplaceInFiles/ReplaceStringInFile.java
+++ ReplaceInFiles/src/ReplaceInFiles/ReplaceStringInFile.java
@@ -6,28 +6,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class ReplaceStringInFile 
+public class ReplaceStringInFile
 {
-	public static void main(String[] args) throws IOException 
-	{
-		String delimiter = args[0];
-		String fileToReadPath = args[1];
-		String fileToWritePath = args[2];
+    public static void main(String[] args) throws IOException
+    {
+        String delimiter = args[0];
+        String fileToReadPath = args[1];
+        String fileToWritePath = args[2];
 
-		File fileToRead = new File(fileToReadPath);
-		
-		try ( Scanner scannerToRead = new Scanner( fileToRead ); 		
-				FileWriter fileToWrite = new FileWriter( fileToWritePath ); ) 
-		{
-	        String oneString = scannerToRead.useDelimiter(delimiter).next().replaceAll("\n", " ");
-	        String twoString = scannerToRead.useDelimiter(delimiter).next();
-	        
-	        fileToWrite.append(oneString);
-	        fileToWrite.append(twoString); 
-		} 
-		catch (FileNotFoundException e) 
-		{
-			System.out.println(e);
-		}
-	}
+        File fileToRead = new File( fileToReadPath );
+
+        try (Scanner scannerToRead = new Scanner( fileToRead );
+                FileWriter fileToWrite = new FileWriter( fileToWritePath );)
+        {
+            String oneString = scannerToRead.useDelimiter( delimiter ).next().replaceAll( "\n", " " );
+            String twoString = scannerToRead.useDelimiter( delimiter ).next();
+
+            fileToWrite.append( oneString );
+            fileToWrite.append( twoString );
+        } catch (FileNotFoundException e)
+        {
+            System.out.println( e );
+        }
+    }
 }
